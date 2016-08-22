37388005f3d510d293c769dc17bc5f0e5af83351
AliLamti <alimahdilamti32@gmail.com>
2016-08-22 14:33:37 +0200

    myFirstCommit

---
+++ alilamti/alilamti.php
@@ -0,0 +1,60 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="alimiseenforme.css"> 
+</head>
+
+
+<body>
+
+
+<div id="corps">
+<form>
+<fieldset id="loula">
+	<legend> Sign Up </legend>	
+</fieldset>
+<fieldset>	
+<div class=ligne>
+<label>Email</label>
+<div class=champs>
+<input type =email name=adresse >
+</div>
+</div>
+
+<div class=ligne>
+<label>First Name</label>
+<div class=champs>
+<input type =text name=nom>
+</div>
+</div>
+
+<div class=ligne>
+<label>Last Name</label>
+<div class=champs>
+<input type =text name=nom>
+</div>
+</div>
+
+<div class=ligne>
+<label>Password</label>
+<div class=champs>
+<input type=password name=nom>
+</div>
+</div>
+
+<div class=champs>
+<input type=submit value="Sign Up">
+</div>
+</fieldset>
+
+<fieldset id="facebook">
+<div class=champs>
+<input type=submit value="Sign Up with facebook">
+</div>
+</fieldset>
+
+
+</form>
+</div>
+</body>
+
+</html>
